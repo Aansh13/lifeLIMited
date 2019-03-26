d23b409a595c01765093063126b6de4ef3f2f2d6
Aansh13 <44342876+Aansh13@users.noreply.github.com>
2019-03-26 13:35:10 +0530

    Update code.java

--- code.java
+++ code.java
@@ -6,21 +6,26 @@ class life
     Scanner sc=new Scanner(System.in);
     int t=sc.nextInt();
     while(t-->0)
-		{	
-			int q=0;
-			String s[]=new String[3]
-			s[0]=sc.nextLine();
-			s[1]=sc.nextLine();
-			s[2]=sc.nextLine();
-	   	for(int i=0;i<2;i++)
-			{		
-				for(int j=0;j<2;j++)
-				{
-					if(s[i].charAt(j)=='l'&&s[i+1].charAt(j)=='l'&&s[i].charAt(j+1)=='l')
-					{
-						q=1;
-					}
+    {	
+	int q=0;
+	String s[]=new String[3]
+	s[0]=sc.nextLine();
+	s[1]=sc.nextLine();
+	s[2]=sc.nextLine();
+	for(int i=0;i<2;i++)
+	{		
+		for(int j=0;j<2;j++)
+		{
+			if(s[i].charAt(j)=='l'&&s[i+1].charAt(j)=='l'&&s[i].charAt(j+1)=='l')
+			{
+				q=1;
 			}
 		}
+	}
+	if(q==1)
+		System.out.println("yes");
+	else
+		System.out.println("no");
+    }
   }
 }
