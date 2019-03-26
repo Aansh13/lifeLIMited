e5af40b5dbced2d12f78fa1746bb58fbe4d8ebfa
Aansh13 <44342876+Aansh13@users.noreply.github.com>
2019-03-26 12:57:56 +0530

    Update code.java

--- code.java
+++ code.java
@@ -1,7 +1,17 @@
 import java.util.*;
 class life
 {
-  public static void main()
+  public static void main(string args[])
   {
+    Scanner sc=new Scanner(System.in);
+    int t=sc.nextInt();
+    while(t-->0)
+	{
+		String s[]=new String[3]
+		s[0]=sc.nextInt();
+		s[1]=sc.nextInt();
+		s[2]=sc.nextInt();
+		
+	}
   }
 }
